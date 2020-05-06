93c858a59b8329cbc04b86b4d4fb27d82753f49c
Vibhav Bobade <vibhav.bobde@gmail.com>
2020-05-06 16:48:30 +0530

    Updates

---
+++ precache-manifest.e91b0c419d35dd4a54f5ff859a5faa6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58865c2bc74632b87130191b380ecbc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "796489e3098f807e8198",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fae0b9e232dcd03fdb71",
+    "url": "/static/js/2.e47477c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e47477c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "796489e3098f807e8198",
+    "url": "/static/js/main.7e6db568.chunk.js"
+  },
+  {
+    "revision": "607fb1d4e8521ab9c184",
+    "url": "/static/js/runtime-main.3f670652.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
