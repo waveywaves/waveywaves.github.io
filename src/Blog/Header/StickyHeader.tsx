cb2ec5098286679c7cb7c0d1420185bcbda9783d
Vibhav Bobade <vibhav.bobde@gmail.com>
2020-05-11 02:41:39 +0530

    Add Options to StickyHeader

--- src/Blog/Header/StickyHeader.tsx
+++ src/Blog/Header/StickyHeader.tsx
@@ -1,10 +1,22 @@
 import React, { Fragment, useEffect, useRef, useState } from 'react';
 import './Header.css';
+import Col from 'react-bootstrap/Col';
 
 function StickyHeader() {
   return (
-    <div className="stickyHeaderInner" >
-        waveywaves
+    <div className="stickyHeaderInner">
+          <a>
+            waveywaves
+          </a>
+          <a>
+            • Work 
+          </a>
+          <a>
+            • Play
+          </a>
+          <a>
+            • Info 
+          </a>
     </div>
   );
 }
