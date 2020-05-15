0d7109028cb760afa6eee77ec909e4f6ad8629d3
Vibhav Bobade <vibhav.bobde@gmail.com>
2020-05-15 22:10:44 +0530

    Refactor Header from function to class

--- src/Blog/Header/Header.tsx
+++ src/Blog/Header/Header.tsx
@@ -6,33 +6,48 @@ import HeaderSketch from './HeaderSketch'
 const P5Wrapper = require("react-p5-wrapper");
 
 
-function Header() {
-  const [isSticky, setSticky] = useState(false);
-  const ref = useRef(null);
-  const handleScroll = () => {
-    if (ref.current) {
-      setSticky(ref.current.getBoundingClientRect().top <= 0.01*window.screen.height);
-    }
-  };
+// function Header() {
+//   const [isSticky, setSticky] = useState(false);
+//   const ref = useRef(null);
+//   const handleScroll = () => {
+//     if (ref.current) {
+//       setSticky(ref.current.getBoundingClientRect().top <= 0.01*window.screen.height);
+//     }
+//   };
 
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+//   useEffect(() => {
+//     window.addEventListener('scroll', handleScroll);
 
-    return () => {
-      window.removeEventListener('scroll', () => handleScroll);
-    };
-  }, []);
+//     return () => {
+//       window.removeEventListener('scroll', () => handleScroll);
+//     };
+//   }, []);
 
-  return (
+//   return (
+  //   <div>
+  //     <div className="Header" >
+  //       waveywaves
+  //     </div>
+  //     <div className={`stickyHeader${isSticky ? ' sticky' : ''}`} ref={ref}>
+  //       <StickyHeader />
+  //     </div>
+  //  </div>
+//   );
+// }
+
+class Header extends React.Component {
+  render(){
+    return (
     <div>
       <div className="Header" >
         waveywaves
       </div>
-      <div className={`stickyHeader${isSticky ? ' sticky' : ''}`} ref={ref}>
-        <StickyHeader />
+      <div className=" ">
+        <StickyHeader/> 
       </div>
    </div>
-  );
+    )
+  }
 }
 
 export default Header;
