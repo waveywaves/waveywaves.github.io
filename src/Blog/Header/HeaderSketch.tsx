3cc4d1226037a3ae281eef5247085cbb0dceffdc
Vibhav Bobade <vibhav.bobde@gmail.com>
2020-05-29 23:48:35 +0530

    Update p5 Sketch for header

--- src/Blog/Header/HeaderSketch.tsx
+++ src/Blog/Header/HeaderSketch.tsx
@@ -6,17 +6,48 @@ import p5 from 'p5'
 var sketch = (p: p5) => {
   var width = window.innerWidth
   var height = window.innerHeight*0.1
+  var r = 0;
+  var g =0;
+  var b = 0;
+  var ascendR = true;
+  var ascendG = true;
+  var ascendB = true;
+
   p.setup = function() {
     p.createCanvas(width, height)
+    p.background(r, g, b);
+    p.colorMode(p.HSL, height, height, height);
   };
 
   p.draw = function() {
-    p.background(0);
-    p.fill(255);
-    p.textSize(100)
+    p.background(r, g, b);
+    r = updateValue(r, ascendR);
+    g = updateValue(g, ascendG);
+    b = updateValue(b, ascendB);
+    p.fill(255-r, 255-g, 255-b);
+    p.textSize(50)
     p.textAlign("center")
     p.text("WAVEYWAVES", width/2, height/2)
   };
+
+  function ascendOrDescend(x: number, ascend: boolean){
+    if (x >= 255){
+      ascend = false
+    }
+    if (x <= 0) {
+      ascend = true
+    }
+    return ascend
+  }
+
+  function updateValue(x: number, ascend: boolean){
+    if (ascendOrDescend(x, ascend)){
+        x++;
+      } else {
+        x--;
+      }
+    return x;
+}
 }
 
 interface IProps {
